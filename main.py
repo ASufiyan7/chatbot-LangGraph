a84e6f230c305770c8a22f9f033f329ba6532ae0
Sufiyan Ansari <ansufiyan17@gmail.com>
2026-01-29 18:52:10 +0530

    added routing logic

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from dotenv import load_dotenv
 
-from langchain_core.messages import HumanMessage, AIMessage, BaseMessage
+from langchain_core.messages import HumanMessage, AIMessage, BaseMessage, SystemMessage
 from langchain_huggingface import HuggingFaceEndpoint
 from langgraph.checkpoint.memory import InMemorySaver
 from langgraph.graph import StateGraph, START, END
@@ -14,7 +14,7 @@ from langgraph.graph.message import add_messages
 load_dotenv()
 HF_TOKEN = os.getenv("HUGGINGFACE_API_KEY")
 
-app = FastAPI(title="LangGraph HuggingFace Chatbot API")
+app = FastAPI(title="LangGraph HuggingFace Chatbot")
 
 llm = HuggingFaceEndpoint(
     repo_id="mistralai/Mistral-7B-Instruct-v0.2",
@@ -26,20 +26,28 @@ llm = HuggingFaceEndpoint(
 class ChatState(TypedDict):
     messages: Annotated[List[BaseMessage], add_messages]
 
-def chat_node(state: ChatState) -> ChatState:
+def thinking_node(state: ChatState) -> ChatState:
+    print("AI is thinking...")
+    system_instruction = SystemMessage(content="You are a helpful assistant.Be concise and clear in your responses.")
+    return {"messages": [system_instruction]}
+
+def generation_node(state: ChatState) -> ChatState:
+    print("Generating AI response...")
     response = llm.invoke(state["messages"])
     return {"messages": [AIMessage(content=response)]}
 
 graph = StateGraph(ChatState)
 
-graph.add_node("chat", chat_node)
-graph.add_edge(START, "chat")
-graph.add_edge("chat", "chat")
+graph.add_node("thinker", thinking_node)
+graph.add_node("generator", generation_node)
 
-app_graph = graph.compile()
+graph.add_edge(START, "thinker")
+graph.add_edge("thinker", "generator")
+graph.add_edge("generator", END)
 
 class ChatRequest(BaseModel):
     message: str
+    thread_id: str = "default_user"
 
 class ChatResponse(BaseModel):
     response: str
@@ -50,15 +58,14 @@ app_graph = graph.compile(checkpointer=memory)
 @app.post("/chat", response_model=ChatResponse)
 def chat(req: ChatRequest):
 
-    config = {"configurable": {"thread_id": "1"}}
+    config = {"configurable": {"thread_id": req.thread_id}}
 
-    input_data = {"messages": [HumanMessage(content=req.message)]}
+    output = app_graph.invoke(
+        {"messages": [HumanMessage(content=req.message)]},
+        config=config
+    )
 
-    output = app_graph.invoke(input_data, config=config)
-
-    ai_message = output["messages"][-1]
-
-    return {"response": ai_message.content}
+    return {'response': output["messages"][-1].content}
 
 @app.get("/")
 def root():
